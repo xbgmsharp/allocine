3369e220ede8afeffa4853d3c03b913ca7bc0f2b
xbgmsharp <xbgmsharp@gmail.com>
2013-05-23 11:24:11 +0900

    Make it work again Add documentation

--- test_api.py
+++ test_api.py
@@ -1,8 +1,15 @@
+import pprint
 from allocine import allocine
+
 api = allocine()
 api.configure('100043982026','29d185d98c984a359e6e6f26a0474269')
 
-movie = api.get(27405)
+search = api.search("Fight club", "movie")
+print "Search result Count [{0}] Code [{1}] Title [{2}]".format(search['feed']['totalResults'],
+                                                                search['feed']['movie'][0]['code'],
+                                                                search['feed']['movie'][0]['originalTitle'])
+
+movie = api.movie(27405)
 print "Movie Title [{0}]".format(movie['movie']['originalTitle'])
 
 serie = api.tvseries(223)
