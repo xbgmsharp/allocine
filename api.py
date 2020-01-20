5ee9e230ec42f0f2c9d53e8d1187a1d34d5351c5
Cedric Rochat <rochatc@gmail.com>
2020-01-20 18:42:44 +0100

    feat(api): Random User-Agent
    Added random User-Agent functionality, to make it even more 403-proof.
    Took UA function from the PHP client.

--- api.py
+++ api.py
@@ -19,6 +19,7 @@ Sample code:
 #from pprint import pprint
 # standard module
 from datetime import date
+import random, time
 import urllib.request, urllib.error, urllib.parse, urllib.request, urllib.parse, urllib.error
 import hashlib, base64
 import json
@@ -35,15 +36,48 @@ class allocine(object):
         self._api_url = 'http://api.allocine.fr/rest/v3'
         self._partner_key  = partner_key
         self._secret_key = secret_key
-        self._user_agent = 'Mozilla/5.0 (Linux; U; Android 1.9; en-gb) AppleWebKit/999+ (KHTML, like Gecko) Safari/999.9'
 
     def configure(self, partner_key=None, secret_key=None):
         """Set the keys"""
         self._partner_key = partner_key
         self._secret_key = secret_key
 
+    def _get_random_user_agent(self):
+        va = random.randrange(1, 4)
+        vb = random.randrange(0, 9)
+        a = random.randrange(0, 9)
+        b = random.randrange(0, 99)
+        c = random.randrange(0, 999)
+
+        userAgents = [f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; fr-fr; Nexus One Build/FRF91) AppleWebKit/5{b}.{c} (KHTML, like Gecko) Version/{a}.{a} Mobile Safari/5{b}.{c}",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; fr-fr; Dell Streak Build/Donut AppleWebKit/5{b}.{c}+ (KHTML, like Gecko) Version/3.{a}.2 Mobile Safari/ 5{b}.{c}.1",
+                    f"Mozilla/5.0 (Linux; U; Android 4.{va}.{vb}; fr-fr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                    f"Mozilla/5.0 (Linux; U; Android 4.{va}.{vb}; fr-fr; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; en-gb) AppleWebKit/999+ (KHTML, like Gecko) Safari/9{b}.{a}",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.5; fr-fr; HTC_IncredibleS_S710e Build/GRJ{b}) AppleWebKit/5{b}.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5{b}.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC Vision Build/GRI{b}) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.4; fr-fr; HTC Desire Build/GRJ{b}) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; T-Mobile myTouch 3G Slide Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.3; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5{b}.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; LG-LU3000 Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5{b}.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC_DesireS_S510e Build/GRI{a}) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/{c}.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.3; fr-fr; HTC Desire Build/GRI{a}) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android 2.{va}.{vb}; fr-fr; HTC Desire Build/FRF{a}) AppleWebKit/533.1 (KHTML, like Gecko) Version/{a}.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; fr-lu; HTC Legend Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/{a}.{a} Mobile Safari/{c}.{a}",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; fr-fr; HTC_DesireHD_A9191 Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.1; fr-fr; HTC_DesireZ_A7{c} Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/{c}.{a}",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}.1; en-gb; HTC_DesireZ_A7272 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/{c}.1",
+                    f"Mozilla/5.0 (Linux; U; Android {va}.{vb}; fr-fr; LG-P5{b} Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"]
+
+        return userAgents[random.randrange(0, len(userAgents) - 1)]
+
     def _do_request(self, method=None, params=None):
         """Generate and send the request"""
+        response = None
+
         # build the URL
         query_url = self._api_url+'/'+method;
 
@@ -67,7 +101,7 @@ class allocine(object):
             try:
                 # do the request
                 req = urllib.request.Request(query_url)
-                req.add_header('User-agent', self._user_agent)
+                req.add_header('User-agent', self._get_random_user_agent())
 
                 str_response = urllib.request.urlopen(req, timeout = 10).readline().decode("utf-8")
                 response = json.loads(str_response)
